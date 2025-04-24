942be9e59a1ac5484c13a929d04c4da4a4f06096
dat.nguyen <dat.nguyen@transperfect.com>
2025-04-24 14:03:28 +0700

    add new fields Category

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -8,5 +8,28 @@ export const Category: CollectionConfig = {
       type: "text",
       required: true,
     },
+    {
+      name: "slug",
+      type: "text",
+      required: true,
+      unique: true,
+    },
+    {
+      name: "color",
+      type: "text",
+    },
+    {
+      name: "parent",
+      type: "relationship",
+      relationTo: "categories",
+      hasMany: false,
+    },
+    {
+      name: "subcategories",
+      type: "join",
+      collection: "categories",
+      on: "parent",
+      hasMany: true,
+    },
   ],
 };
